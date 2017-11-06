46bea9e957eba0abd94e33696f5e68f81d2769e6
Zack Cuddy <zcuddy23@gmail.com>
2017-11-06 12:17:12 -0600

    ng-changes work for scanners, need to set up models

--- public/js/controllers/createController.js
+++ public/js/controllers/createController.js
@@ -1,23 +1,30 @@
 rfidApp.controller('createController', ['$scope', function($scope) {
 
-    $scope.gameType = "Standard";
     $scope.numLocations = 1;
     $scope.numScanners = 1;
     $scope.locNames = [];
-
-    var locationTemplate = {
-        id: 1,
-        name: ""
-    };
-
-    $scope.locNames.push(locationTemplate);
-    $scope.selectedLoc = $scope.locNames[0];
 
     $scope.gameTypes = [
         "Standard",
         "Voting",
         "Question"
     ];
+
+    var locationTemplate = {
+        id: 1,
+        name: "",
+        numScanners: 1,
+        scanners: [
+            {
+                id: 1,
+                text: "",
+                type: $scope.gameTypes[0]
+            }
+        ]
+    };
+
+    $scope.locNames.push(locationTemplate);
+    $scope.selectedLoc = $scope.locNames[0];
 
     $scope.updateLocations = function(newValue, oldValue) {
         var changeValue = newValue - oldValue;
@@ -26,7 +33,15 @@ rfidApp.controller('createController', ['$scope', function($scope) {
             while (counter <= newValue) {
                 var locationTemplate = {
                     id: counter,
-                    name: ""
+                    name: "",
+                    numScanners: 1,
+                    scanners: [
+                        {
+                            id: 1,
+                            text: "",
+                            type: $scope.gameTypes[0]
+                        }
+                    ]
                 };
 
                 $scope.locNames.push(locationTemplate);
@@ -43,4 +58,32 @@ rfidApp.controller('createController', ['$scope', function($scope) {
         }
     };
 
+    $scope.updateScanners = function(newValue, oldValue) {
+        var changeValue = newValue - oldValue;
+        if (changeValue >= 0) {
+            var counter = Number(oldValue) + 1;
+            while (counter <= newValue) {
+                var scannerTemplate = {
+                    id: counter,
+                    text: "",
+                    type: $scope.gameTypes[0]
+                };
+
+                $scope.selectedLoc.scanners.push(scannerTemplate);
+                counter++
+            }
+        } else {
+            for (var i = 0; i < -changeValue; i++) {
+                $scope.selectedLoc.scanners.pop();
+            }
+        }
+
+        if (!$scope.selectedScanner.id || $scope.selectedScanner.scanner.id > newValue) {
+            $scope.selectedScanner = $scope.selectedLoc.scanners[newValue - 1];
+        }
+    };
+
+    $scope.$watch("selectedLoc", function() {
+        $scope.selectedScanner = $scope.selectedLoc.scanners[0];
+    })
 }]);
