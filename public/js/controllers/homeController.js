bd3c035bd40c53810705f9ec76024e5322d1a424
Zack Cuddy <zcuddy23@gmail.com>
2017-11-27 13:59:07 -0600

    loader on home page

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -1,8 +1,10 @@
 rfidApp.controller('homeController', ['$scope', 'CurrentGame', function($scope, CurrentGame) {
     $scope.gameRunning = false;
+    $scope.loading = true;
 
     CurrentGame.get().$promise.then(function success(data) {
         console.log(data);
+        $scope.loading = false;
         if (data.active) {
             $scope.gameRunning = true;
         }
---
+++ public/js/directives/loader/loader.js
@@ -0,0 +1,5 @@
+rfidApp.directive("loader", function() {
+    return {
+        templateUrl: "/assets/js/directives/loader/loader.html"
+    };
+});
