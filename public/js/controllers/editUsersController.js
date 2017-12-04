ffcbc6d481db3357f27338c886af752773535495
Zack Cuddy <zcuddy23@gmail.com>
2017-12-04 15:04:31 -0600

    List users

--- public/js/controllers/editUsersController.js
+++ public/js/controllers/editUsersController.js
@@ -1,15 +1,31 @@
-rfidApp.controller('editUsersController', ['$scope', 'CurrentGame', function($scope, CurrentGame) {
+rfidApp.controller('editUsersController', ['$scope', '$http', 'CurrentGame', function($scope, $http, CurrentGame) {
     $scope.gameRunning = false;
     $scope.loading = true;
 
     CurrentGame.get().$promise.then(function success(data) {
         console.log(data);
-        $scope.loading = false;
+        $scope.game = data;
         if (data.active) {
             $scope.gameRunning = true;
+            getUsers();
         }
+        $scope.loading = false;
     }, function error(res) {
         console.log("ERROR " + res);
         $scope.error = true;
     });
+
+    var getUsers = function() {
+        $http({
+            method: "GET",
+            url: "/api/users",
+            params: {gameId: $scope.game._id}
+        }).then(function success(res) {
+            console.log(res);
+            $scope.users = res.data;
+        }, function error(res) {
+            console.log("ERROR " + res);
+            $scope.error = true;
+        });
+    }
 }]);
