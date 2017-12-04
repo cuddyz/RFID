93f90fc9c2d3c23df30d65f804ab1751d2940c9c
Zack Cuddy <zcuddy23@gmail.com>
2017-12-04 13:42:32 -0600

    Sort previous games from most recent

--- public/js/controllers/previousGamesController.js
+++ public/js/controllers/previousGamesController.js
@@ -7,6 +7,7 @@ rfidApp.controller('previousGamesController', ['$scope', '$http', function($scop
     }).then(function success(res) {
         console.log(res);
         $scope.games = res.data;
+        sortGames();
         formatDates();
         $scope.loading = false;
     }, function error(res) {
@@ -14,10 +15,15 @@ rfidApp.controller('previousGamesController', ['$scope', '$http', function($scop
         $scope.error = true;
     });
 
+    var sortGames = function() {
+        $scope.games.sort(function(a,b){
+            return new Date(b.created) - new Date(a.created);
+        });
+    };
+
     var formatDates = function() {
         for (var i = 0; i < $scope.games.length; i++) {
             $scope.games[i].created = moment($scope.games[i].created).format('MMMM Do YYYY, h:mm:ss a');
         }
-    }
-
+    };
 }]);
