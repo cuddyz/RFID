2f0a3e1afbde0dc92c2b6032c573180f0c96579e
Zack Cuddy <zcuddy23@gmail.com>
2017-12-04 13:40:04 -0600

    List previous games

--- public/js/controllers/previousGamesController.js
+++ public/js/controllers/previousGamesController.js
@@ -1,3 +1,23 @@
-rfidApp.controller('previousGamesController', ['$scope', function($scope) {
-    $scope.loading = false;
+rfidApp.controller('previousGamesController', ['$scope', '$http', function($scope, $http) {
+    $scope.loading = true;
+
+    $http({
+        method: "GET",
+        url: "/api/inactive-games"
+    }).then(function success(res) {
+        console.log(res);
+        $scope.games = res.data;
+        formatDates();
+        $scope.loading = false;
+    }, function error(res) {
+        console.log("ERROR " + res);
+        $scope.error = true;
+    });
+
+    var formatDates = function() {
+        for (var i = 0; i < $scope.games.length; i++) {
+            $scope.games[i].created = moment($scope.games[i].created).format('MMMM Do YYYY, h:mm:ss a');
+        }
+    }
+
 }]);
